f9c05630131a5c48ea8a17cae9c233f1e5552c34
Diana <di.makar0va@yandex.ru>
2025-10-20 12:09:51 +0300

    Удаление лишних комментариев

--- src/main/java/ru/Diana/NauJava/NauJavaApplication.java
+++ src/main/java/ru/Diana/NauJava/NauJavaApplication.java
@@ -3,11 +3,6 @@ package ru.Diana.NauJava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Главный класс Spring Boot приложения для управления событиями
- * Практическая работа 3 - Spring Framework
- */
-
 @SpringBootApplication(scanBasePackages = "ru.Diana")
 public class NauJavaApplication {
 
